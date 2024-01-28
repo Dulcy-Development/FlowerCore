19631cd79e6f8c1cb0eecb4aa84ef7a2f9e144bb
Emmiesa <skynetworkdotnet@gmail.com>
2024-01-28 11:18:42 +0100

    refactor SetSpawnCommand.java

--- src/main/java/me/emmiesa/flowercore/plugin/register.java
+++ src/main/java/me/emmiesa/flowercore/plugin/register.java
@@ -8,7 +8,7 @@ import me.emmiesa.flowercore.commands.admin.gamemode.gmspCommand;
 import me.emmiesa.flowercore.commands.admin.rank.GrantCommand;
 import me.emmiesa.flowercore.commands.admin.rank.RankCommand;
 import me.emmiesa.flowercore.commands.admin.rank.SubCommands.*;
-import me.emmiesa.flowercore.commands.admin.spawn.SetSpawnCommand;
+import me.emmiesa.flowercore.commands.admin.spawn.SetJoinLocation;
 import me.emmiesa.flowercore.commands.admin.spawn.TeleportSpawnCommand;
 import me.emmiesa.flowercore.commands.global.*;
 import me.emmiesa.flowercore.commands.global.PluginCommand;
@@ -71,7 +71,7 @@ public class register {
         new AnnounceCommand();
         new InstanceCommand();
         new TeleportCommand();
-        new SetSpawnCommand();
+        new SetJoinLocation();
         new ClearChatCommand();
         new TeleportUpCommand();
         new TeleportHereCommand();
