6b186af9cd44f0c5d4d2f1c371f9cd936f579986
Emmiesa <skynetworkdotnet@gmail.com>
2024-02-03 14:39:55 +0100

    option to disable /say & /me and a bypass permission

---
+++ src/main/java/me/emmiesa/flowercore/commandblocker/CommandListener.java
@@ -0,0 +1,22 @@
+package me.emmiesa.flowercore.commandblocker;
+
+import me.emmiesa.flowercore.FlowerCore;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+public class CommandListener implements Listener {
+
+    @EventHandler
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+        if (!event.getPlayer().hasPermission(FlowerCore.instance.getConfig("settings.yml").getString("vanilla-commands-blocker.perm-to-bypass"))) {
+            if (FlowerCore.instance.getConfig("settings.yml").getBoolean("vanilla-commands-blocker.disable_me-say")) {
+                if (event.getMessage().toLowerCase().startsWith("/me ") || event.getMessage().toLowerCase().equals("/me") ||
+                        event.getMessage().toLowerCase().startsWith("/say ") || event.getMessage().toLowerCase().equals("/say")) {
+                    event.setCancelled(true);
+                    event.getPlayer().sendMessage("Unknown command. Type \"/help\" for help.");
+                }
+            }
+        }
+    }
+}
