229edcde2b8b23c94b945320f217d6341cb45b31
Emmiesa <skynetworkdotnet@gmail.com>
2024-02-03 13:40:24 +0100

    Grant confirm menu

---
+++ src/main/java/me/emmiesa/flowercore/menus/grantconfirm/GrantConfirmMenu.java
@@ -0,0 +1,57 @@
+package me.emmiesa.flowercore.menus.grantconfirm;
+
+import me.emmiesa.flowercore.menus.grantconfirm.button.GrantConfirmButton;
+import me.emmiesa.flowercore.ranks.Rank;
+import me.emmiesa.flowercore.utils.chat.CC;
+import me.emmiesa.flowercore.utils.menu.Button;
+import me.emmiesa.flowercore.utils.menu.Menu;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class GrantConfirmMenu extends Menu {
+    private final UUID playerToGrantUUID;
+    private final Rank rank;
+    private final String playerName;
+
+    public GrantConfirmMenu(UUID playerToGrantUUID, Rank rank, String playerName) {
+        this.playerToGrantUUID = playerToGrantUUID;
+        this.rank = rank;
+        this.playerName = playerName;
+    }
+
+    @Override
+    public String getTitle(Player player) {
+        return CC.translate("Confirm Grant for " + playerName);
+    }
+
+    @Override
+    public Map<Integer, Button> getButtons(Player player) {
+        Map<Integer, Button> buttons = new HashMap<>();
+
+        buttons.put(11, new GrantConfirmButton(playerToGrantUUID, rank, Material.DIAMOND_BLOCK, "&b&lConfirm", Arrays.asList(
+                " &fTo be granted Player &b" + playerName,
+                " &fTo be granted Rank: &b" + rank.getDisplayName(),
+                rank.isStaff() ? " &fRank Type: &bStaff" : " &fRank Type: &bNon-Staff/Donator",
+                " ",
+                "&bClick to confirm the grant!"
+        ), true));
+
+        buttons.put(15, new GrantConfirmButton(playerToGrantUUID, rank, Material.REDSTONE_BLOCK, "&c&lCancel", Arrays.asList(
+                " ",
+                "&cClick to cancel the grant!",
+                " "
+        ), false));
+
+        return buttons;
+    }
+
+    @Override
+    public int getSize() {
+        return 9 * 3;
+    }
+}
