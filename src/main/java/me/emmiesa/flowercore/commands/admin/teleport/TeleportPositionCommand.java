12b33701caeb8a96299930ce11e1ac6d953ab3aa
Emmiesa <skynetworkdotnet@gmail.com>
2024-02-04 15:41:19 +0100

    added aliases to TeleportPositionCommand

--- src/main/java/me/emmiesa/flowercore/commands/admin/teleport/TeleportPositionCommand.java
+++ src/main/java/me/emmiesa/flowercore/commands/admin/teleport/TeleportPositionCommand.java
@@ -10,33 +10,33 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 public class TeleportPositionCommand extends BaseCommand {
-    @Command(name = "teleportposition", permission = "flowercore.command.tppos", aliases = {"tp-pos"})
+    @Command(name = "teleportposition", permission = "flowercore.command.tppos", aliases = {"tp-pos", "tppos"})
 
     @Override
     public void onCommand(CommandArgs cmd) {
-        Player p = cmd.getPlayer();
+        Player player = cmd.getPlayer();
         String[] args = cmd.getArgs();
 
         if (args.length < 3) {
-            p.sendMessage("§cUsage: /" + cmd.getLabel() + " <x> <y> <z>");
+            player.sendMessage("§cUsage: /" + cmd.getLabel() + " (x) (y) (z)");
             return;
         }
         if (!NumberUtils.checkInt(args[0]) || !NumberUtils.checkInt(args[1]) || !NumberUtils.checkInt(args[2])) {
-            p.sendMessage("§ePlease specify a number");
+            player.sendMessage("§ePlease specify a number");
             return;
         }
 
         int x = Integer.parseInt(args[0]);
         int y = Integer.parseInt(args[1]);
         int z = Integer.parseInt(args[2]);
-        Location loc = new Location(p.getWorld(), x, y, z);
+        Location loc = new Location(player.getWorld(), x, y, z);
         if (x > 30000000 || x < -30000000 || y > 30000000 || y < -30000000 || z > 30000000 || z < -30000000) {
-            p.sendMessage("§eMax value is 30000000/-30000000");
+            player.sendMessage("§eMax value is 30000000/-30000000");
             return;
         }
 
-        p.teleport(loc);
-        p.sendMessage(CC.translate(FlowerCore.instance.getConfig("messages.yml").getString("teleport.tp-pos")
+        player.teleport(loc);
+        player.sendMessage(CC.translate(FlowerCore.instance.getConfig("messages.yml").getString("teleport.tp-pos")
                 .replace("%x%", String.valueOf(x))
                 .replace("%y%", String.valueOf(y))
                 .replace("%z%", String.valueOf(z)))
