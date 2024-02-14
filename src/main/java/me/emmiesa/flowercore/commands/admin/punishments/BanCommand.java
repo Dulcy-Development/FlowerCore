ef91a830d89dbeda66fa2ecd5e13303c1edcbb03
Emmiesa <skynetworkdotnet@gmail.com>
2024-02-14 08:20:50 +0100

    default reason & duration when banning a player and the args are not entered

--- src/main/java/me/emmiesa/flowercore/commands/admin/punishments/BanCommand.java
+++ src/main/java/me/emmiesa/flowercore/commands/admin/punishments/BanCommand.java
@@ -13,30 +13,29 @@ import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
-
 public class BanCommand extends BaseCommand {
 
-    @Command(name = "ban", inGameOnly = false, permission = "flower.punishment.ban")
+    @Command(name = "ban", /*inGameOnly = false,*/ permission = "flower.punishment.ban")
     public void onCommand(CommandArgs args) {
         Player player = args.getPlayer();
 
-        if (args.length() < 3) {
-            player.sendMessage(CC.translate("&cUsage: /ban (player) (reason) (duration) (-s / -c)"));
+        if (args.length() < 1) {
+            player.sendMessage(CC.translate("&cUsage: /ban (player) (reason) (duration) [-s / -c]"));
             return;
         }
 
         String target = args.getArgs(0);
-        String reason = args.getArgs(1);
-        String duration = args.getArgs(2);
+        String reason = args.length() > 1 ? args.getArgs(1) : "No reason given"; // default reason - if only /Ban (player) is executed
+        String duration = args.length() > 2 ? args.getArgs(2) : "permanent"; // default duration default - if only /Ban (player) is executed :)
         String silentornot = args.length() > 3 ? args.getArgs(3) : ""; //makes -s or -c optional so you can either enter it or not
 
         Player bannedBy = args.getPlayer();
-        UUID playerUUID = player.getUniqueId();
+        /*UUID playerUUID = player.getUniqueId();*/
 
-        banplayer(target, reason, duration, silentornot, bannedBy, playerUUID);
+        banplayer(target, reason, duration, silentornot, bannedBy/*, playerUUID*/);
     }
 
-    public void banplayer(String playerName, String reason, String duration, String silentornot, Player bannedBy, UUID bannedByUUID) {
+    public void banplayer(String playerName, String reason, String duration, String silentornot, Player bannedBy/*, UUID bannedByUUID*/) {
         Player targetPlayer = Bukkit.getServer().getPlayer(playerName);
         if (targetPlayer == null) {
             bannedBy.sendMessage(CC.translate("&cPlayer not found!"));
@@ -46,6 +45,6 @@ public class BanCommand extends BaseCommand {
         Punishment punishment = new Punishment(UUID.randomUUID(), bannedBy.getUniqueId(), PunishmentType.BAN, reason, targetPlayer.getAddress().getAddress().getHostAddress(), silentornot.equalsIgnoreCase("-s"));
         FlowerCore.getInstance().getPlayerManager().addPunishment(targetPlayer.getUniqueId(), punishment);
 
-        Utils.broadcastMessage(CC.translate(bannedBy.getDisplayName() + " has banned " + targetPlayer.getName() + " because of " + reason + " for " + duration));
+        Utils.broadcastMessage(CC.translate(bannedBy.getDisplayName() + " has banned " + targetPlayer.getName() + " for " + reason + ". Duration: " + duration + (silentornot.equalsIgnoreCase("-s") ? " [Silently]" : "")));
     }
 }
