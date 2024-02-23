3a3b8d322f5d5fc66db05f2c1e2c24fd10fd76b5
Emmiesa <skynetworkdotnet@gmail.com>
2024-02-23 18:13:11 +0100

    new blacklist command

---
+++ src/main/java/me/emmiesa/flowercore/commands/admin/punishments/BlacklistCommand.java
@@ -0,0 +1,49 @@
+package me.emmiesa.flowercore.commands.admin.punishments;
+
+import me.emmiesa.flowercore.FlowerCore;
+import me.emmiesa.flowercore.punishments.Punishment;
+import me.emmiesa.flowercore.punishments.PunishmentType;
+import me.emmiesa.flowercore.utils.Utils;
+import me.emmiesa.flowercore.utils.chat.CC;
+import me.emmiesa.flowercore.utils.command.BaseCommand;
+import me.emmiesa.flowercore.utils.command.Command;
+import me.emmiesa.flowercore.utils.command.CommandArgs;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
+
+public class BlacklistCommand extends BaseCommand {
+
+    @Command(name = "blacklist", permission = "flower.punishment.blacklist")
+    public void onCommand(CommandArgs args) {
+        Player player = args.getPlayer();
+
+        if (args.length() < 1) {
+            player.sendMessage(CC.translate("&cUsage: /blacklist (player) (reason) (duration) [-s]"));
+            return;
+        }
+
+        String target = args.getArgs(0);
+        String reason = args.length() > 1 ? args.getArgs(1) : "No reason given";
+        String duration = args.length() > 2 ? args.getArgs(2) : "permanent";
+        String silentornot = args.length() > 3 ? args.getArgs(3) : "";
+
+        Player bannedBy = args.getPlayer();
+        //UUID playerUUID = player.getUniqueId();
+
+        Player targetPlayer = FlowerCore.instance.getServer().getPlayer(target);
+        if (target == null) {
+            bannedBy.sendMessage(CC.translate("&cPlayer not found!"));
+            return;
+        }
+
+        Punishment punishment = new Punishment(UUID.randomUUID(), bannedBy.getUniqueId(), PunishmentType.BLACKLIST, reason, targetPlayer.getAddress().getAddress().getHostAddress(), silentornot.equalsIgnoreCase("-s"));
+        FlowerCore.getInstance().getPlayerManager().addPunishment(targetPlayer.getUniqueId(), punishment);
+
+        targetPlayer.kickPlayer(CC.translate(FlowerCore.getInstance().getConfig("messages.yml").getString("punishments.blacklist").replace("%punisher%", Bukkit.getOfflinePlayer(punishment.getBy()).getName()).replace("%reason%", punishment.getReason())));
+        //targetPlayer.kickPlayer(CC.translate("&cYou have been punished! \n&fPunish Type: &c" + punishment.getType().toString().toLowerCase() + "\n&fPunished By: &c" + Bukkit.getOfflinePlayer(punishment.getBy()).getName() + "\n&fReason: &c" + punishment.getReason() + "\n"));
+        Utils.broadcastMessage(CC.translate(bannedBy.getDisplayName() + " has blacklisted " + targetPlayer.getName() + " for " + reason + ". Duration: " + duration + (silentornot.equalsIgnoreCase("-s") ? " [Silently]" : "")));
+
+    }
+}
