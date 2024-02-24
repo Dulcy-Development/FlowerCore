0d02cb3c07e2e8f6ba31239dc215f63058a6d453
Emmiesa <skynetworkdotnet@gmail.com>
2024-02-24 14:30:18 +0100

    Troll Commands: Launch, Trap, Capture

---
+++ src/main/java/me/emmiesa/flowercore/commands/admin/troll/LaunchCommand.java
@@ -0,0 +1,35 @@
+package me.emmiesa.flowercore.commands.admin.troll;
+
+import me.emmiesa.flowercore.utils.chat.CC;
+import me.emmiesa.flowercore.utils.command.BaseCommand;
+import me.emmiesa.flowercore.utils.command.Command;
+import me.emmiesa.flowercore.utils.command.CommandArgs;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+public class LaunchCommand extends BaseCommand {
+    @Command(name = "launch", inGameOnly = false, permission = "flowercore.command.launch")
+    public void onCommand(CommandArgs args) {
+        CommandSender sender = args.getSender();
+        if (args.length() > 0) {
+            Player target = Bukkit.getPlayerExact(args.getArgs(0));
+            if (target == null) {
+                sender.sendMessage(CC.translate("&cPlayer not found."));
+                return;
+            }
+            target.setVelocity(new Vector(0, 1, 0).multiply(15));
+            target.sendMessage(CC.translate("&aYou have been launched into the air!"));
+        } else {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(CC.translate("&cUsage: /launch (player)"));
+                return;
+            }
+            Player player = (Player) sender;
+            player.setVelocity(new Vector(0, 1, 0).multiply(15));
+            player.sendMessage(CC.translate("&aYou have been launched into the air!"));
+        }
+    }
+
+}
---
+++ src/main/java/me/emmiesa/flowercore/commands/admin/troll/TrapCommand.java
@@ -0,0 +1,56 @@
+package me.emmiesa.flowercore.commands.admin.troll;
+
+import me.emmiesa.flowercore.utils.chat.CC;
+import me.emmiesa.flowercore.utils.command.BaseCommand;
+import me.emmiesa.flowercore.utils.command.Command;
+import me.emmiesa.flowercore.utils.command.CommandArgs;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class TrapCommand extends BaseCommand {
+    @Command(name = "trap", inGameOnly = false, permission = "flowercore.command.trap")
+    public void onCommand(CommandArgs args) {
+        CommandSender sender = args.getSender();
+        Player target;
+
+        if (args.length() > 0) {
+            target = Bukkit.getPlayerExact(args.getArgs(0));
+            if (target == null) {
+                sender.sendMessage(CC.translate("&cPlayer not found."));
+                return;
+            }
+        } else {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(CC.translate("&cUsage: /trap (player)"));
+                return;
+            }
+            target = (Player) sender;
+        }
+
+        Location loc = target.getLocation();
+        World world = target.getWorld();
+
+        int px = loc.getBlockX();
+        int py = loc.getBlockY();
+        int pz = loc.getBlockZ();
+
+        int[][] positions = {
+                {1, 0, 0}, {-1, 0, 0}, {0, 0, 1}, {0, 0, -1},
+                {1, 1, 0}, {-1, 1, 0}, {0, 1, 1}, {0, 1, -1},
+                {1, 2, 0}, {-1, 2, 0}, {0, 2, 1}, {0, 2, -1},
+                {0, -1, 0}, {0, 3, 0}
+        };
+
+        for (int[] pos : positions) {
+            world.getBlockAt(px + pos[0], py + pos[1], pz + pos[2]).setType(Material.GLASS);
+        }
+
+        sender.sendMessage(CC.translate("&bYou've trapped &3" + target.getDisplayName() + " &bin glass. They can probably no longer move! \n&cIf they escaped, try using /capture (player)"));
+        target.sendMessage(CC.translate("&cYou've been trapped by &4" + args.getPlayer().getDisplayName() + "&c. You're in trouble (!)"));
+    }
+}
+
