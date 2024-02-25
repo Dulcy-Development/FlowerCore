1c568a2a48317af4bbe4dc59bf04f07158249b72
Emmiesa <skynetworkdotnet@gmail.com>
2024-02-25 11:43:40 +0100

    Improved CommandListener.java: Get to be blocked commands from the config

--- src/main/java/me/emmiesa/flowercore/listeners/CommandListener.java
+++ src/main/java/me/emmiesa/flowercore/listeners/CommandListener.java
@@ -4,17 +4,23 @@ import me.emmiesa.flowercore.FlowerCore;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import java.util.List;
 
 public class CommandListener implements Listener {
 
     @EventHandler
     public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
         if (!event.getPlayer().hasPermission(FlowerCore.getInstance().getConfig("settings.yml").getString("vanilla-commands-blocker.perm-to-bypass"))) {
-            if (FlowerCore.getInstance().getConfig("settings.yml").getBoolean("vanilla-commands-blocker.disable_me-say")) {
-                if (event.getMessage().toLowerCase().startsWith("/me ") || event.getMessage().equalsIgnoreCase("/me") ||
-                        event.getMessage().toLowerCase().startsWith("/say ") || event.getMessage().equalsIgnoreCase("/say")) {
+
+            List<String> blockedCommands = FlowerCore.getInstance().getConfig("settings.yml").getStringList("vanilla-commands-blocker.disable_commands");
+
+            for (String command : blockedCommands) {
+                String formattedCommand = command.startsWith("/") ? command : "/" + command;
+
+                if (event.getMessage().toLowerCase().startsWith(formattedCommand.toLowerCase())) {
                     event.setCancelled(true);
                     event.getPlayer().sendMessage("Unknown command. Type \"/help\" for help.");
+                    break;
                 }
             }
         }
