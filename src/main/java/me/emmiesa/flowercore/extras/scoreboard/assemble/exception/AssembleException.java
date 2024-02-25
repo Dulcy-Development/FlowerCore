203ca0be3d820d3b99b5215a10e7858056ef888b
Emmiesa <skynetworkdotnet@gmail.com>
2024-02-25 10:55:08 +0100

    remove a useless extra

--- src/main/java/me/emmiesa/flowercore/extras/scoreboard/assemble/exception/AssembleException.java
+++
@@ -1,8 +0,0 @@
-package me.emmiesa.flowercore.extras.scoreboard.assemble.exception;
-
-public class AssembleException extends RuntimeException {
-
-    public AssembleException(String message) {
-        super(message);
-    }
-}
