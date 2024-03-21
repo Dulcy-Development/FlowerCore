0b2a1fcbadd784f0978acb0e88e055cdd7919c52
Emmiesa <skynetworkdotnet@gmail.com>
2024-03-21 22:06:08 +0100

    Add more info to Instance command

--- src/main/java/me/emmiesa/flowercore/commands/admin/server/InstanceCommand.java
+++ src/main/java/me/emmiesa/flowercore/commands/admin/server/InstanceCommand.java
@@ -7,8 +7,12 @@ import me.emmiesa.flowercore.utils.command.BaseCommand;
 import me.emmiesa.flowercore.utils.command.Command;
 import me.emmiesa.flowercore.utils.command.CommandArgs;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Emmy
@@ -31,6 +35,18 @@ public class InstanceCommand extends BaseCommand {
             pluginsList.append(plugin.getName());
         }
 
+        // Count ops regardless of whether they are online or not
+        int opCount = 0;
+        List<String> opNames = new ArrayList<>();
+        for (OfflinePlayer offlinePlayer : Bukkit.getServer().getOfflinePlayers()) {
+            if (offlinePlayer.isOp()) {
+                opCount++;
+                opNames.add(offlinePlayer.getName());
+            }
+        }
+
+        int pluginCount = Bukkit.getServer().getPluginManager().getPlugins().length;
+
         for (String message : FlowerCore.getInstance().getConfig("messages.yml").getStringList("instance-command.lines")) {
             sender.sendMessage(CC.translate(message)
                     .replace("%server-region%", Locale.SERVER_REGION)
@@ -40,8 +56,10 @@ public class InstanceCommand extends BaseCommand {
                     .replace("%spigot%", Bukkit.getServer().getName())
                     .replace("%max-players%", String.valueOf(Bukkit.getServer().getMaxPlayers()))
                     .replace("%online-players%", String.valueOf(Bukkit.getServer().getOnlinePlayers().size()))
-                    .replace("%plugins%", pluginsList.toString())
-            );
+                    .replace("%plugins-name%", pluginsList.toString())
+                    .replace("%plugins%", String.valueOf(pluginCount))
+                    .replace("%ops%", String.valueOf(opCount))
+                    .replace("%ops-name%", String.join(CC.translate(separator), opNames)));
         }
     }
 }
