f1d19ec704f41b0230b01ce66c8a7b73136b8bb9
Emmy <lisariesey@gmail.com>
2024-08-02 09:29:17 +0200

    fix: manually need to register every single command because flowercore is a bitch

--- src/main/java/me/emmy/core/FlowerCore.java
+++ src/main/java/me/emmy/core/FlowerCore.java
@@ -2,37 +2,80 @@ package me.emmy.core;
 
 import lombok.Getter;
 import lombok.Setter;
-
-import me.emmy.core.api.command.BaseCommand;
 import me.emmy.core.api.command.CommandFramework;
 import me.emmy.core.api.menu.listener.MenuListener;
 import me.emmy.core.chat.ChatRepository;
+import me.emmy.core.chat.command.ClearChatCommand;
+import me.emmy.core.chat.command.MuteChatCommand;
+import me.emmy.core.chat.command.UnMuteChatCommand;
 import me.emmy.core.chat.listener.ChatListener;
+import me.emmy.core.command.FlowerCoreCommand;
+import me.emmy.core.command.admin.essential.*;
+import me.emmy.core.command.admin.gamemode.gmaCommand;
+import me.emmy.core.command.admin.gamemode.gmcCommand;
+import me.emmy.core.command.admin.gamemode.gmsCommand;
+import me.emmy.core.command.admin.gamemode.gmspCommand;
+import me.emmy.core.command.admin.management.*;
+import me.emmy.core.command.admin.server.InstanceCommand;
+import me.emmy.core.command.admin.teleport.*;
+import me.emmy.core.command.admin.troll.*;
+import me.emmy.core.command.donator.AnnounceCommand;
+import me.emmy.core.command.global.server.JoinCommand;
+import me.emmy.core.command.global.server.PingCommand;
+import me.emmy.core.command.global.worldtime.DayCommand;
+import me.emmy.core.command.global.worldtime.NightCommand;
+import me.emmy.core.command.global.worldtime.SunsetCommand;
 import me.emmy.core.config.ConfigHandler;
 import me.emmy.core.conversation.ConversationHandler;
+import me.emmy.core.conversation.command.MessageCommand;
+import me.emmy.core.conversation.command.ReplyCommand;
 import me.emmy.core.database.MongoService;
 import me.emmy.core.essential.godmode.GodModeMemory;
+import me.emmy.core.essential.godmode.command.GodModeCommand;
 import me.emmy.core.essential.godmode.listener.GodModeListener;
 import me.emmy.core.grant.GrantHandler;
+import me.emmy.core.grant.command.GrantCommand;
+import me.emmy.core.grant.command.GrantsCommand;
 import me.emmy.core.listener.CommandListener;
+import me.emmy.core.news.command.NewsCommand;
 import me.emmy.core.papi.PlaceholderAPI;
 import me.emmy.core.profile.ProfileRepository;
 import me.emmy.core.profile.listener.ProfileListener;
+import me.emmy.core.punishment.command.PunishHistoryCommand;
+import me.emmy.core.punishment.command.pardon.UnMuteCommand;
+import me.emmy.core.punishment.command.pardon.UnbanCommand;
+import me.emmy.core.punishment.command.pardon.UnblacklistCommand;
+import me.emmy.core.punishment.command.punish.BanCommand;
+import me.emmy.core.punishment.command.punish.BlacklistCommand;
+import me.emmy.core.punishment.command.punish.KickCommand;
+import me.emmy.core.punishment.command.punish.MuteCommand;
 import me.emmy.core.rank.RankRepository;
+import me.emmy.core.rank.command.RankCommand;
+import me.emmy.core.rank.command.SetRankCommand;
+import me.emmy.core.rank.command.SetRankPurchasedCommand;
+import me.emmy.core.rank.command.impl.*;
+import me.emmy.core.setting.command.SettingsCommand;
+import me.emmy.core.setting.command.toggle.ToggleGlobalChatCommand;
+import me.emmy.core.setting.command.toggle.TogglePrivateMessageSounds;
+import me.emmy.core.setting.command.toggle.TogglePrivateMessagesCommand;
+import me.emmy.core.socials.command.*;
 import me.emmy.core.spawn.SpawnHandler;
+import me.emmy.core.spawn.command.SetJoinLocation;
+import me.emmy.core.spawn.command.TeleportSpawnCommand;
 import me.emmy.core.tag.TagRepository;
+import me.emmy.core.tag.command.TagAdminCommand;
+import me.emmy.core.tag.command.TagCommand;
+import me.emmy.core.tag.command.impl.*;
 import me.emmy.core.tip.TipsHandler;
 import me.emmy.core.utils.Cooldown;
 import me.emmy.core.utils.chat.CC;
 import me.emmy.core.utils.chat.Logger;
-
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.util.Set;
 
 /**
  * Dulcy Development @ 2023 - 2024
@@ -158,7 +201,108 @@ public class FlowerCore extends JavaPlugin {
     private void registerCommands() {
         long start = System.currentTimeMillis();
 
-        commandFramework.registerCommandsInPackage("me.emmy.core");
+        //commandFramework.registerCommandsInPackage("me.emmy.core");  no idea why this doesnt fucking work
+        new FlowerCoreCommand();
+        new FlyCommand();
+        new gmcCommand();
+        new gmsCommand();
+        new gmaCommand();
+        new InfoCommand();
+        new gmspCommand();
+        new HealCommand();
+        new JoinCommand();
+        new PingCommand();
+        new MoreCommand();
+        new TrapCommand();
+        new SudoCommand();
+        new NewsCommand();
+        new TrollCommand();
+        new StoreCommand();
+        new SpeedCommand();
+        new ReplyCommand();
+        new CraftCommand();
+        new ClearCommand();
+        new TikTokCommand();
+        new LaunchCommand();
+        new RocketCommand();
+        new RenameCommand();
+        new RebootCommand();
+        new FakeOpCommand();
+        new CaptureCommand();
+        new YouTubeCommand();
+        new DiscordCommand();
+        new MessageCommand();
+        new TwitterCommand();
+        new WebsiteCommand();
+        new SettingsCommand();
+        new AnnounceCommand();
+        new InstanceCommand();
+        new TeleportCommand();
+        new SetJoinLocation();
+        new LocationCommand();
+        new BroadcastCommand();
+        new TeamSpeakCommand();
+        new TeleportUpCommand();
+        new TrollSilentCommand();
+        new TeleportAllCommand();
+        new TeleportHereCommand();
+        new TeleportSpawnCommand();
+        new PunishHistoryCommand();
+        new TrollEverybodyCommand();
+        new TeleportPositionCommand();
+
+        new ClearChatCommand();
+        new MuteChatCommand();
+        new UnMuteChatCommand();
+
+        new TagCommand();
+        new TagListCommand();
+        new TagSaveCommand();
+        new TagAdminCommand();
+        new TagCreateCommand();
+        new TagSettagCommand();
+        new TagDeleteCommand();
+        new TagSetDisplayCommand();
+
+        new RankCommand();
+        new GrantCommand();
+        new GrantsCommand();
+        new SetRankCommand();
+        new RankListCommand();
+        new RankSaveCommand();
+        new RankCreateCommand();
+        new RankDeleteCommand();
+        new RankSetIconCommand();
+        new RankSetStaffCommand();
+        new RankSetColorCommand();
+        new RankSetSuffixCommand();
+        new RankSetPrefixCommand();
+        new RankSetDisplayCommand();
+        new RankSetDefaultCommand();
+        new RankSetPriorityCommand();
+        new SetRankPurchasedCommand();
+        new RankAddPermissionsCommand();
+
+        new ToggleGlobalChatCommand();
+        new TogglePrivateMessageSounds();
+        new TogglePrivateMessagesCommand();
+
+        new BanCommand();
+        new AltsCommand();
+        new KickCommand();
+        new MuteCommand();
+        new BlacklistCommand();
+
+        new UnbanCommand();
+        new UnMuteCommand();
+        new UnblacklistCommand();
+
+        new DayCommand();
+        new NightCommand();
+        new SunsetCommand();
+
+        new AlertCommand();
+        new GodModeCommand();
 
         long end = System.currentTimeMillis();
         long timeTaken = end - start;
